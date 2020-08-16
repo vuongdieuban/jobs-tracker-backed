4771be15b7b00c48be2a68f7d889c015990ca9d6
ban vuong <vuongdieuban@gmail.com>
2020-08-15 19:49:44 -0600

    add some comment on how to push data out to connected client in socket gateway

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -6,11 +6,24 @@ import {
   WebSocketGateway,
   WsResponse
 } from '@nestjs/websockets';
+import { JobPostStateService } from './job-post/job-post-state.service';
+
+interface ConnectedSockets {
+  [key: string]: any[];
+}
 
 @WebSocketGateway()
 export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private readonly logger: Logger = new Logger('AppGateway');
 
+  private readonly connectedSockets: ConnectedSockets = {};
+
+  constructor(private readonly jobpostStateService: JobPostStateService) {
+    this.jobpostStateService.data$.subscribe((data) => {
+      this.logger.log(`Rcv data in socket gateway ${data}`);
+      // go thru connectedSockets and push info to client
+    });
+  }
 
   handleDisconnect(client: any) {
     this.logger.log('Client Disconnected');
@@ -18,6 +31,7 @@ export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   handleConnection(client: any, ...args: any[]) {
     this.logger.log('Client Connected');
+    // Add connected client to the connectedSocket map
   }
 
   @SubscribeMessage('msgToServer')
