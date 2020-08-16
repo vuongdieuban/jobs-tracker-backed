67eda7e1f2ea54025d517d36543ffcff6285f96a
ban vuong <vuongdieuban@gmail.com>
2020-08-16 15:37:37 -0600

    add client html to test socket

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,3 +1,5 @@
+// https://medium.com/@mohsenes/websocket-cluster-with-nestjs-and-redis-a18882d418ed
+
 import { Logger } from '@nestjs/common';
 import {
   OnGatewayConnection,
@@ -6,36 +8,37 @@ import {
   WebSocketGateway,
   WsResponse
 } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { JobPostStateService } from './job-post/job-post-state.service';
 
-interface ConnectedSockets {
-  [key: string]: any[];
+interface ConnectedClientSockets {
+  [key: string]: Socket[]; // userId: [socketClient1, socketClient2, ...]
 }
 
 @WebSocketGateway()
 export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   private readonly logger: Logger = new Logger('AppGateway');
 
-  private readonly connectedSockets: ConnectedSockets = {};
+  private readonly connectedSockets: ConnectedClientSockets = {};
 
   constructor(private readonly jobpostStateService: JobPostStateService) {
     this.jobpostStateService.data$.subscribe((data) => {
       this.logger.log(`Rcv data in socket gateway: ${data}`);
-      // go thru connectedSockets and push info to client
+      // go thru connectedSockets and push info to client (socketClient.send(payload))
     });
   }
 
-  handleDisconnect(client: any) {
+  handleDisconnect(client: Socket) {
     this.logger.log('Client Disconnected');
   }
 
-  handleConnection(client: any, ...args: any[]) {
-    this.logger.log('Client Connected');
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client Connected: ${client.id}`);
     // Add connected client to the connectedSocket map
   }
 
   @SubscribeMessage('msgToServer')
-  handleMessage(client: any, payload: any): WsResponse<string> {
+  handleMessage(client: Socket, payload: any): WsResponse<string> {
     return { event: 'msgToClient', data: 'Hello from server' };
   }
 }
