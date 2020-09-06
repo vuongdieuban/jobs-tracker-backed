fdd7135f084cb7bd132dbe9cb55e94dd3a8e9354
ban vuong <vuongdieuban@gmail.com>
2020-09-05 22:28:57 -0600

    boilerplate for job application controller and service

--- src/job-application/job-application.module.ts
+++ src/job-application/job-application.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobApplicationEntity } from './entities/job-application.entity';
+import { JobApplicationController } from './job-application.controller';
+import { JobApplicationService } from './job-application.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([JobApplicationEntity])],
-  controllers: [],
-  providers: [],
+  controllers: [JobApplicationController],
+  providers: [JobApplicationService],
   exports: []
 })
 export class JobApplicationModule {}
