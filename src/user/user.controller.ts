e20ba3cce62b7c332bce984e26be2f33e3fadde6
ban vuong <vuongdieuban@gmail.com>
2020-09-14 22:49:37 -0600

    get all user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './entities/user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+@ApiTags('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/')
+  public async findAll(): Promise<UserEntity[]> {
+    return this.userService.findAll();
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepo: Repository<UserEntity>
+  ) {}
+
+  public async findAll(): Promise<UserEntity[]> {
+    return this.userRepo.find();
+  }
+}
