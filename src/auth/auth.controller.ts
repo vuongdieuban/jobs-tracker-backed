a4d42918995a498063482adf41915d300e169151
ban vuong <vuongdieuban@gmail.com>
2020-09-26 15:41:50 -0600

    small edit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,12 +5,11 @@ import { Request, Response } from 'express';
 export class AuthController {
   @Get('/login')
   public async login(@Req() request: Request, @Res() response: Response) {
-    console.log('RESQUEST', request.cookies);
     const localEnv = process.env.NODE_ENV === 'development';
-    const token = 'abc';
+    const token = 'abcdef';
     response.cookie('refresh_token', token, {
       domain: 'jobs-tracker.localhost', // Needed for cookie to appear in browser
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       sameSite: localEnv ? 'lax' : 'none', // Needed for Chrome. Make none (with secure) if subdomain can't be set
       secure: !localEnv, // Makes cookie need https. required for sameSite.None
       httpOnly: true // Cookie can't be accessed by javaScript
