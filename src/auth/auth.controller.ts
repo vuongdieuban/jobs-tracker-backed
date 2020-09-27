5a229ea4e60730acebeff07cd36f06c0fcee6d66
ban vuong <vuongdieuban@gmail.com>
2020-09-27 10:05:33 -0600

    add googleapis

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,10 +5,11 @@ import { Request, Response } from 'express';
 export class AuthController {
   @Get('/login')
   public async login(@Req() request: Request, @Res() response: Response) {
+    // const body = req.body
     const localEnv = process.env.NODE_ENV === 'development';
     const token = 'rtyq';
     response.cookie('refresh_token', token, {
-      domain: 'jobs-tracker.localhost', // Needed for cookie to appear in browser
+      domain: '.jobs-tracker.localhost', // Needed for cookie to appear in browser
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       sameSite: localEnv ? 'lax' : 'none', // Needed for Chrome. Make none (with secure) if subdomain can't be set
       secure: !localEnv, // Makes cookie need https. required for sameSite.None
